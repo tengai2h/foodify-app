d3c1a62cf3f48ebf6571b80ed80c2a733352b1f2
Your Name <you@example.com>
2021-12-28 13:50:56 +0200

    Updates

--- src/constants/routes.ts
+++
@@ -1,4 +0,0 @@
-export const ROUTES = {
-  MAIN: '/main',
-  FAVORITES: '/favorites',
-};
--- src/redux/cards/actionTypes.ts
+++
@@ -1,3 +0,0 @@
-export const GET_CARDS = 'GET_CARDS';
-export const SET_FAVORITES_CARDS = 'SET_FAVORITES_CARDS';
-export const ADD_NEW_DISH = 'ADD_NEW_DISH';
